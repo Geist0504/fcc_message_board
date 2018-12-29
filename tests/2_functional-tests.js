bf3958b5dfec1f7734f447bf781e1e2f0d8073ca
Glitch (hot-mechanic) <none>
2018-12-29 19:17:05 +0000

    👗🌹 Checkpoint ./tests/2_functional-tests.js:2025103/1319 ./routes/api.js:2025103/104

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,7 +10,7 @@ var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
 var server = require('../server');
-let test_
+let test_board = 'test'
 
 chai.use(chaiHttp);
 
@@ -18,29 +18,21 @@ suite('Functional Tests', function() {
 
   suite('API ROUTING FOR /api/threads/:board', function() {
     
-    suite('POST', function(done) {
-      chai.request(server)
-        .post('/api/issues/general')
-        .send({
-          issue_title: 'Title',
-          issue_text: 'text',
-          created_by: 'Functional Test - Every field filled in',
-          assigned_to: 'Chai and Mocha',
-          status_text: 'In QA'
-        })
-        .end(function(err, res){
-          assert.equal(res.status, 200);
-          assert.equal(res.body.issue_title, 'Title')
-          assert.equal(res.body.issue_text, 'text')
-          assert.equal(res.body.created_by, 'Functional Test - Every field filled in')
-          assert.equal(res.body.assigned_to, 'Chai and Mocha')
-          assert.equal(res.body.status_text, 'In QA')
-          assert.equal(res.body.open, true)
-          assert.equal(new Date(res.body.created_on).setHours(0,0,0,0), new Date().setHours(0,0,0,0))
-          assert.equal(new Date(res.body.updated_on).setHours(0,0,0,0), new Date().setHours(0,0,0,0))
-          done();
-        });
-      
+    suite('POST', function() {
+      test('Every field filled in', function(done) {
+        chai.request(server)
+          .post('/api/threads/' +  test_board)
+          .send({
+            text: 'This is my test post',
+            delete_password: 'delete'
+          })
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            console.log(res.redirects)
+            assert.equal(res.redirects[0][-7:-1], '/b/test')
+            done();
+          });
+      })
     });
     
     suite('GET', function() {
