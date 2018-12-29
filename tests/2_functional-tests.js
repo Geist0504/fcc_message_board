e649af30eb36191881bf90eb9bb3abb71792be03
Glitch (hot-mechanic) <none>
2018-12-29 19:06:05 +0000

    🎈🏔 Checkpoint ./tests/2_functional-tests.js:2025103/1057

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,6 +10,7 @@ var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
 var server = require('../server');
+let test_
 
 chai.use(chaiHttp);
 
@@ -17,7 +18,28 @@ suite('Functional Tests', function() {
 
   suite('API ROUTING FOR /api/threads/:board', function() {
     
-    suite('POST', function() {
+    suite('POST', function(done) {
+      chai.request(server)
+        .post('/api/issues/general')
+        .send({
+          issue_title: 'Title',
+          issue_text: 'text',
+          created_by: 'Functional Test - Every field filled in',
+          assigned_to: 'Chai and Mocha',
+          status_text: 'In QA'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.issue_title, 'Title')
+          assert.equal(res.body.issue_text, 'text')
+          assert.equal(res.body.created_by, 'Functional Test - Every field filled in')
+          assert.equal(res.body.assigned_to, 'Chai and Mocha')
+          assert.equal(res.body.status_text, 'In QA')
+          assert.equal(res.body.open, true)
+          assert.equal(new Date(res.body.created_on).setHours(0,0,0,0), new Date().setHours(0,0,0,0))
+          assert.equal(new Date(res.body.updated_on).setHours(0,0,0,0), new Date().setHours(0,0,0,0))
+          done();
+        });
       
     });
     
