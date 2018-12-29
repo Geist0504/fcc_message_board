7b2fa80193a7b9e1f63f74459c774d13fccb3889
Glitch (hot-mechanic) <none>
2018-12-29 18:23:03 +0000

    🏵🍧 Checkpoint ./server.js:2025103/497 ./routes/api.js:2025103/809

--- routes/api.js
+++ routes/api.js
@@ -10,10 +10,46 @@
 
 var expect = require('chai').expect;
 
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+const MONGODB_CONNECTION_STRING = process.env.DB;
+
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+  
+    .get(function (req, res){
+
+    })
+
+    .post(function (req, res){
+
+    })
+
+    .put(function (req, res){
+
+    })
+
+    .delete(function (req, res){
+
+    })
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+  
+    .get(function (req, res){
+
+    })
+
+    .post(function (req, res){
+
+    })
+
+    .put(function (req, res){
+
+    })
+
+    .delete(function (req, res){
+
+    })
 
 };
